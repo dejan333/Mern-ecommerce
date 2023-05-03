dc96014c1860e25d3ee3fb1a5e23dc11559f8784
Dejan Gjorgievski <gjorgievskidejan91@gmail.com>
2023-05-03 17:56:07 +0200

    Product details page

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,20 +1,20 @@
-import { Row, Col} from 'react-bootstrap'
-import products from '../products'
-import Product from '../components/Product'
+import { Row, Col } from "react-bootstrap";
+import products from "../products";
+import Product from "../components/Product";
 
 function HomeScreen() {
   return (
     <>
-    <h1>Latest Products</h1>
-    <Row>
-      {products.map((product) => (
-        <Col sm="12" md="6" lg="4" xl="3">
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col key={product._id} sm="12" md="6" lg="4" xl="3">
             <Product product={product} />
-        </Col>
-      ))}
-    </Row>
+          </Col>
+        ))}
+      </Row>
     </>
-  )
+  );
 }
 
-export default HomeScreen
+export default HomeScreen;
