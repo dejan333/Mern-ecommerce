43b4eea34f0311f306f11740bccdf084c32d79b0
Dejan Gjorgievski <gjorgievskidejan91@gmail.com>
2023-06-10 17:39:08 +0200

    Update user profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -81,13 +81,48 @@ const logoutUser = asyncHandler(async (req, res) => {
 //route   GET /api/users/profile
 //acess   Private
 const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await Users.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      message: "message",
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
   res.send("get user profile");
 });
 //desc    Update user profile
 //route   PUT /api/users/profile
 //acess   Private/
 const updateUserProfile = asyncHandler(async (req, res) => {
-  res.send("update user profile");
+  const user = await Users.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updateUser = await user.save();
+
+    res.json({
+      _id: updateUser._id,
+      name: updateUser.name,
+      email: updateUser.email,
+      isAdmin: updateUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 //desc    Get all users
