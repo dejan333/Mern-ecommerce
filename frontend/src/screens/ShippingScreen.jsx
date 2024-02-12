a38da48d524f5f7f2909b65046dbb8d9df475615
unknown <gjorgievskidejan91@gmail.com>
2024-02-12 22:06:29 +0100

    Checout Steps

--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -5,7 +5,7 @@ import { saveShippingAddress } from '../slices/cartSlice';
 import { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import FormContainer from '../components/FormContainer';
-
+import CheckoutSteps from '../components/CheckoutSteps';
 
 
 const ShippingScreen = () => {
@@ -28,6 +28,7 @@ const ShippingScreen = () => {
 
   return (
     <FormContainer>
+      <CheckoutSteps step1 step2 />
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group className='my-2' controlId='address'>
@@ -78,8 +79,11 @@ const ShippingScreen = () => {
           Continue
         </Button>
       </Form>
+      
     </FormContainer>
+    
   );
+  
 };
 
 export default ShippingScreen;
