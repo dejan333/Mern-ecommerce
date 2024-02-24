4912c429b72f99b3b763f0feb7790ed0e9f09654
unknown <gjorgievskidejan91@gmail.com>
2024-02-24 18:07:59 +0100

    Display Order history on Profile Screen

--- frontend/src/slices/ordersApiSlice.js
+++ frontend/src/slices/ordersApiSlice.js
@@ -1,13 +1,13 @@
-import { apiSlice } from './apiSlice';
-import { ORDERS_URL, PAYPAL_URL } from '../constants';
+import { apiSlice } from "./apiSlice";
+import { ORDERS_URL, PAYPAL_URL } from "../constants";
 
 export const orderApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     createOrder: builder.mutation({
       query: (order) => ({
         url: ORDERS_URL,
-        method: 'POST',
-        body: {...order},
+        method: "POST",
+        body: { ...order },
       }),
     }),
     getOrderDetails: builder.query({
@@ -19,17 +19,29 @@ export const orderApiSlice = apiSlice.injectEndpoints({
     payOrder: builder.mutation({
       query: ({ orderId, details }) => ({
         url: `${ORDERS_URL}/${orderId}/pay`,
-        method: 'PUT',
+        method: "PUT",
         body: details,
-    }),
+      }),
     }),
     getPaypalClientId: builder.query({
       query: () => ({
         url: PAYPAL_URL,
       }),
-      keepUnusedDataFor:5,
-    })
+      keepUnusedDataFor: 5,
+    }),
+    getMyOrders: builder.query({
+      query: () => ({
+        url: `${ORDERS_URL}/mine`,
+      }),
+      keepUnusedDataFor: 5,
+    }),
   }),
 });
 
-export const { useCreateOrderMutation, useGetOrderDetailsQuery,useGetPaypalClientIdQuery,usePayOrderMutation } = orderApiSlice;
+export const {
+  useCreateOrderMutation,
+  useGetOrderDetailsQuery,
+  useGetPaypalClientIdQuery,
+  usePayOrderMutation,
+  useGetMyOrdersQuery,
+} = orderApiSlice;
